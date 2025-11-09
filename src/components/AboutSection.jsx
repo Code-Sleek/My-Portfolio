d16c879fec5006e7375aab6cf73e045e6b503090
Yashodha Cooray <hcooray2003@gmail.com>
2025-11-10 01:35:36 +0530

    CV

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -31,7 +31,7 @@ export const AboutSection = () => {
                         </a>
 
                         <a 
-                        href="/Projects/CV.pdf" 
+                        href="/Projects/CV - Yashodha Cooray.pdf" 
                         download = "Yashodha_Cooray.pdf"
                         className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
                             Download CV
